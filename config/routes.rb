375ea1453b9434b8fb7772fa79fc2e888141dea8
Ben Vishny <Ben@ben-vishnys-computer.local>
2009-03-21 20:49:31 -0500

    message

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
-  map.connect 'jsgen/:action/:ref.js' 
+  map.connect 'jsgen/:action/:ref.js', :controller => 'jsgen'
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
