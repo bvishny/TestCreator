9ccde434d8d73ebf9e1864df041416cf766b9c0f
Ben Vishny <Ben@ben-vishnys-computer.local>
2009-05-05 20:28:32 -0500

    m

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -388,14 +388,14 @@ class QuizController < ApplicationController
   	item = Item.find_by_ref(params[:id])
   	taken = @user.takens.find(:first, :conditions => ["quiz_id = ?", item.quiz.id])
   	if ! taken.blank?
-  	 require 'net/http'
-      url = URI.parse(item.c4)
-      req = Net::HTTP::Get.new(url.path)
-      res = Net::HTTP.start(url.host, url.port) {|http|
-      	http.request(req)
-       }
-    	send_data res.body, :disposition => 'inline'
+  	 #require 'net/http'
+     # url = URI.parse(item.c4)
+     # req = Net::HTTP::Get.new(url.path)
+     # res = Net::HTTP.start(url.host, url.port) {|http|
+     # 	http.request(req)
+      # }
+    #	send_data res.body, :disposition => 'inline'
     end
-  	render :text => item.quiz.id
+  	render :text => item.c4
   end
 end
