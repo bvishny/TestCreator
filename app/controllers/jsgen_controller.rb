c3e9c782a505ba765e3109d3a4b415f7a0cee04b
Ben Vishny <Ben@ben-vishnys-computer.local>
2009-05-25 13:36:28 -0500

    m

--- app/controllers/jsgen_controller.rb
+++ app/controllers/jsgen_controller.rb
@@ -1,13 +1,16 @@
 class JsgenController < ApplicationController
   before_filter :auth
+  before_filter :content_type
   def results
     @quiz = @user.quizzes.find_by_ref(params[:ref])
     @takens = @quiz.takens.find(:all, :conditions => ["status = 1 and quiz_id = ?", @quiz.id])
-    headers['Content-Type'] = 'text/javascript'
   end
   def editor
     @quiz = @user.quizes.find_by_ref(params[:ref])
     @items = @quiz.items.find(:all, :conditions => ["order_index ASC"])
+  end
+  private
+  def content_type
     headers['Content-Type'] = 'text/javascript'
   end
 end
