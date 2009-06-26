eaa28f195bddbeaa8a981427308102bd3f72e672
Ben Vishny <Ben@ben-vishnys-computer.local>
2009-06-25 23:04:13 -0500

    m

--- db/migrate/20091227222509_add_role copy.rb
+++
@@ -1,8 +0,0 @@
-class AddRole < ActiveRecord::Migration
-  def self.up
-   add_column :users, :role, :integer, :default => 0
-  end
-
-  def self.down
-  end
-end
--- db/migrate/add_time_zone.rb
+++
@@ -1,8 +0,0 @@
-class AddTimeZone < ActiveRecord::Migration
-  def self.up
-   add_column :users, :time_zone, :string, :default => "UTC"
-  end
-
-  def self.down
-  end
-end
