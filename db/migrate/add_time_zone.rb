73a20fb2d285be997137f088596c8ab1390f6c88
Ben Vishny <Ben@ben-vishnys-computer.local>
2009-06-25 22:46:45 -0500

    m

--- db/migrate/add_time_zone.rb
+++ db/migrate/add_time_zone.rb
@@ -1,6 +1,6 @@
 class AddTimeZone < ActiveRecord::Migration
   def self.up
-   add_column :users, :time_zone, :integer, :default => 0
+   add_column :users, :time_zone, :string, :default => "UTC"
   end
 
   def self.down
